6e54ce02681c6cc1fcec11d3e53fcf8068b9d32c
Maria Clara Matos <mariaclaramatos086@gmail.com>
2026-07-13 17:48:15 -0300

    Order, Instant, ISO 8601 PART 1

---
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -0,0 +1,78 @@
+package com.educandoweb.course.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "td_order")
+public class Order implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Instant moment;
+    // private OrderStatus orderStatus;
+
+    @ManyToOne // Associação de chave estrangeira
+    @JoinColumn(name = "client_id")
+    private User client;
+
+    public Order(){}
+
+    public Order(Integer id, Instant moment, User client) {
+        this.client = client;
+        this.id = id;
+        this.moment = moment;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "client=" + client +
+                ", id=" + id +
+                ", moment=" + moment +
+                '}';
+    }
+}
